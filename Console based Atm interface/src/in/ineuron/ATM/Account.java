0fa146c80fad350f94c2a502b0ce704478225bb5
Rishi <arishi2200@gmail.com>
2023-05-17 18:03:52 +0530

    First Commit
    Uploading Files

---
+++ Console based Atm interface/src/in/ineuron/ATM/Account.java
@@ -0,0 +1,15 @@
+package in.ineuron.ATM;
+
+class Account {
+	private String id = "rv123";
+	private String pin = "1234";
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPin() {
+		return pin;
+	}
+
+}
